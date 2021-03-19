7757209c32c8aeffa27fe94111d8384c3c3ece53
lljmb <brown.jessm@icloud.com>
2021-03-19 19:02:17 -0400

    working on tests

--- api/task/model.js
+++ api/task/model.js
@@ -2,19 +2,41 @@
 const db = require("../../data/dbConfig")
 
 const getTaskById = (id) => {
-    return db('tasks')
+    const tasks = db('tasks')
         .where('task_id', id)
+        .select('task_completed', 'task_description', 'task_notes')
+
+        tasks.task_completed === 0 ? tasks.task_completed = false : tasks.task_completed = true;
+
+    return tasks
+ 
 }
 
 module.exports = {
     getTasks() {
-        return db('tasks as t')
+        const tasks = db('tasks as t')
             .join('projects as p', 'p.project_id', 't.project_id')
             .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+
+        const result = tasks.map(task => {
+            if(task.task_completed === 1){
+                return { 
+                    ...task,
+                    task_completed: true }
+            } else {
+                return {
+                ...task,
+                task_completed: false
+                }
+            }
+        });   
+
+        return result     
     },
 
     addTask(task) {
-        return db('tasks').insert(task)
+        return db('tasks')
+        .insert(task)
         .then(id=> { 
             return getTaskById(id)
         })
